1f22b8cb60fd9d11392f86687df9b71745ed8fb5
Ingo Karkat <swdev@ingo-karkat.de>
2022-11-01 21:28:17 +0100

    completions: Define gitCompleteAs and add some checkout-alike completions
    Git is clever enough to figure out a simple alias co = checkout, but once the git-wrapper is involved, this doesn't work any longer. Same for custom commands like inout.
    If there's a function _git_COMMAND(), Git will automatically invoke that. We can use that to define simple forwarding completion wrappers.

--- shell/completions.sh
+++ shell/completions.sh
@@ -2,3 +2,21 @@
 
 completeAsCommand git-autostash git-inside \
     git-brrefdo git-localbrcdo git-localbrcrefdo git-localbrdo git-localbrrefdo git-rbrrefdo git-subIdo git-subdo git-subido git-substdo git-wc-with-suffix-do git-wcdo git-wcs-in-dir-do
+
+gitCompleteAs()
+{
+    typeset sourceCommand="${1:?}"; shift
+    typeset cmd; for cmd
+    do
+	eval "_git_${cmd}() { _git_${sourceCommand} \"\$@\"; }"
+    done
+}
+
+gitCompleteAs checkout co \
+    inout iofiles iosubmodules io ab \
+    oinout oiofiles oiosubmodules oio oab \
+    uinout uiofiles uiosubmodules uio uab \
+    ominout omiofiles omiosubmodules omio omab \
+    uminout umiofiles umiosubmodules umio umab
+
+unset -f gitCompleteAs

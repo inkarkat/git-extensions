ccc091a6a20e6f9f778ea255ef9d633bfb632c34
Ingo Karkat <swdev@ingo-karkat.de>
2022-11-17 12:25:43 +0100

    Refactoring: Use if..then

--- profile/aliases.sh
+++ profile/aliases.sh
@@ -11,23 +11,35 @@
 # different user (through withUnixhome), because git-userlocalconfig updates
 # information. To avoid the error (and instead write the actual user's
 # config), use the USER_HOME if provided by withUnixhome.
-exists meld	&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithmeld 'customtool diff.tool meld' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithmeld 'customtool merge.tool meld'
-exists kdiff3   && HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithkdiff3 'customtool diff.tool kdiff3' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithkdiff3 'customtool merge.tool kdiff3'
-exists tkdiff   && HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithtkdiff 'customtool diff.tool tkdiff' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithtkdiff 'customtool merge.tool tkdiff'
-exists vimdiff  && HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithvimdiff 'customtool diff.tool vimdiff' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithvimdiff 'customtool merge.tool vimdiff' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithvimdiff2 'customtool diff.tool vimdiff2' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithvimdiff2 'customtool merge.tool vimdiff2' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithvimdiff3 'customtool diff.tool vimdiff3' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithvimdiff3 'customtool merge.tool vimdiff3'
-exists gvimdiff && HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithgvimdiff 'customtool diff.tool gvimdiff' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithgvimdiff 'customtool merge.tool gvimdiff' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithgvimdiff2 'customtool diff.tool gvimdiff2' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithgvimdiff2 'customtool merge.tool gvimdiff2' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithgvimdiff3 'customtool diff.tool gvimdiff3' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithgvimdiff3 'customtool merge.tool gvimdiff3'
-exists xxdiff   && HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithxxdiff 'customtool diff.tool xxdiff' \
-		&& HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithxxdiff 'customtool merge.tool xxdiff'
+if exists meld; then
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithmeld 'customtool diff.tool meld'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithmeld 'customtool merge.tool meld'
+fi
+if exists kdiff3; then
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithkdiff3 'customtool diff.tool kdiff3'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithkdiff3 'customtool merge.tool kdiff3'
+fi
+if exists tkdiff; then
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithtkdiff 'customtool diff.tool tkdiff'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithtkdiff 'customtool merge.tool tkdiff'
+fi
+if exists vimdiff; then
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithvimdiff 'customtool diff.tool vimdiff'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithvimdiff 'customtool merge.tool vimdiff'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithvimdiff2 'customtool diff.tool vimdiff2'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithvimdiff2 'customtool merge.tool vimdiff2'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithvimdiff3 'customtool diff.tool vimdiff3'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithvimdiff3 'customtool merge.tool vimdiff3'
+fi
+if exists gvimdiff; then
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithgvimdiff 'customtool diff.tool gvimdiff'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithgvimdiff 'customtool merge.tool gvimdiff'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithgvimdiff2 'customtool diff.tool gvimdiff2'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithgvimdiff2 'customtool merge.tool gvimdiff2'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithgvimdiff3 'customtool diff.tool gvimdiff3'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithgvimdiff3 'customtool merge.tool gvimdiff3'
+fi
+if exists xxdiff; then
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.diffwithxxdiff 'customtool diff.tool xxdiff'
+    HOME="${USER_HOME:-$HOME}" git-userlocalconfig alias.mergewithxxdiff 'customtool merge.tool xxdiff'
+fi

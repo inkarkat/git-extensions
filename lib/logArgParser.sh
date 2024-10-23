91653b1dde8fff24820cc2da81b34200cb6d66f5
Ingo Karkat <swdev@ingo-karkat.de>
2024-10-23 12:20:29 +0200

    logArgParser: Refactoring: Extract countDiffArgs()

--- lib/logArgParser.sh
+++ lib/logArgParser.sh
@@ -5,21 +5,11 @@ countLogArgs()
 {
     local isIncludeDiffArgs="${1?}"; shift
 
-    [ "$isIncludeDiffArgs" ] \
-	&& case "$1" in
-	    -+([RWXabcpuw]))
-		    return 1;;
-	    --@(cc|color-moved|color-words|color|compact-summary|cumulative|dd|default-prefix|dirstat|dirstat-by-file|ext-diff|full-diff|function-context|histogram|ignore-all-space|ignore-blank-lines|ignore-cr-at-eol|ignore-space-at-eol|ignore-space-change|ignore-submodules|ita-invisible-in-index|minimal|no-color|no-color-moved|no-color-moved-ws|no-ext-diff|no-prefix|no-relative|no-textconv|numstat|patch|patch-with-raw|patch-with-stat|patience|relative|remerge-diff|shortstat|stat|text|textconv|word-diff))
-		    return 1;;
-	    -[IOU])
-		    return 2;;
-	    --@(anchored|color-moved-ws|diff-algorithm|diff-merges|dst-prefix|ignore-matching-lines|inter-hunk-context|line-prefix|rotate-to|skip-to|src-prefix|unified|word-diff-regex)=*)
-		    return 1;;
-	    --@(anchored|color-moved-ws|diff-algorithm|diff-merges|dst-prefix|ignore-matching-lines|inter-hunk-context|line-prefix|rotate-to|skip-to|src-prefix|unified|word-diff-regex))
-		    return 2;;
-	esac
+    if [ "$isIncludeDiffArgs" ]; then
+	countDiffArgs "$1" || return $?
+    fi
 
-    case "$1" in
+    case "${1?}" in
 	# Note: Exclude -N because that's taken by git-*-command(s).
 	# +([0-9])
 	-+([BCDEFMPgimstz]))
@@ -31,6 +21,24 @@ countLogArgs()
 	--@(after|author|before|committer|date|decorate-refs-exclude|decorate-refs|diff-filter|encoding|exclude-hidden|exclude|expand-tabs|find-object|format|glob|grep-reflog|grep|max-count|max-parents|min-parents|output-indicator-context|output-indicator-new|output-indicator-old|output|since-as-filter|since|skip|until|ws-error-highlight)=*)
 		return 1;;
 	--@(after|author|before|committer|date|decorate-refs-exclude|decorate-refs|diff-filter|encoding|exclude-hidden|exclude|expand-tabs|find-object|format|glob|grep-reflog|grep|max-count|max-parents|min-parents|output-indicator-context|output-indicator-new|output-indicator-old|output|since-as-filter|since|skip|until|ws-error-highlight))
+		return 2;;
+    esac
+
+    return 0
+}
+
+countDiffArgs()
+{
+    case "${1?}" in
+	-+([RWXabcpuw]))
+		return 1;;
+	--@(cc|color-moved|color-words|color|compact-summary|cumulative|dd|default-prefix|dirstat|dirstat-by-file|ext-diff|full-diff|function-context|histogram|ignore-all-space|ignore-blank-lines|ignore-cr-at-eol|ignore-space-at-eol|ignore-space-change|ignore-submodules|ita-invisible-in-index|minimal|no-color|no-color-moved|no-color-moved-ws|no-ext-diff|no-prefix|no-relative|no-textconv|numstat|patch|patch-with-raw|patch-with-stat|patience|relative|remerge-diff|shortstat|stat|text|textconv|word-diff))
+		return 1;;
+	-[IOU])
+		return 2;;
+	--@(anchored|color-moved-ws|diff-algorithm|diff-merges|dst-prefix|ignore-matching-lines|inter-hunk-context|line-prefix|rotate-to|skip-to|src-prefix|unified|word-diff-regex)=*)
+		return 1;;
+	--@(anchored|color-moved-ws|diff-algorithm|diff-merges|dst-prefix|ignore-matching-lines|inter-hunk-context|line-prefix|rotate-to|skip-to|src-prefix|unified|word-diff-regex))
 		return 2;;
     esac
 

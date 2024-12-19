116af559b09fece5a7d853432fea114cb7bb746e
Ingo Karkat <swdev@ingo-karkat.de>
2024-12-19 09:38:49 +0100

    aggregate-range: FIX: Missed edit
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: git version 2.47.1, hub version 2.14.2, gh version 2.55.0 (2024-08-20), git-lfs/3.6.0 (GitHub; linux amd64; go 1.23.1)

--- lib/br-variants/aggregate-range.sh
+++ lib/br-variants/aggregate-range.sh
@@ -102,7 +102,7 @@ l?(h|g|og)?(except)by|\
 @(@(log?(v)|show)@(last|first)|@(l?(o)g?(v)|count))@(g|changed|touched)?(except)by|\
 l?(o)g?([fv]|merges)?(except)by|\
 @(l?(o)|count|countmaxdaycommits|commitsperday|log@(distribution|msgstat)|l?(o)gtitleg|brlifetimes)?(except)by|\
-activityby\
+activity?(except)by\
 )
 	[ "$gitCommand" = lgby ] && gitCommand='onelinelog'
 	othersCommand "$@"

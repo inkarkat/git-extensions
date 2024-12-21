1d447703c7fed75b47461dcb110631da4dfb85d1
Ingo Karkat <swdev@ingo-karkat.de>
2024-12-21 16:44:41 +0100

    ENH: Add repomove br... variants
    For moving the entire current branch, or today's commits, etc.
    
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: git version 2.47.1, hub version 2.14.2, gh version 2.55.0 (2024-08-20), git-lfs/3.6.0 (GitHub; linux amd64; go 1.23.1)

--- lib/br-variants/branch-range.sh
+++ lib/br-variants/branch-range.sh
@@ -97,6 +97,8 @@ subchanges|superchanges|subrevl@(?(o)g|c)\
 	$EXEC git-branch-command "${branchCommandAdditionalArgs[@]}" --keep-position "${scopeCommand[@]}" ${scopeCommand:+--keep-position} rev-range --revision "${scopeRevision:?}" --end-revision "${scopeEndRevision:?}" --keep-position selectedcommit-command --single-only --range-is-last -3 "$gitCommand" COMMITS RANGE "$@";;
     ma)
 	$EXEC git-branch-command "${branchCommandAdditionalArgs[@]}" --keep-position "${scopeCommand[@]}" ${scopeCommand:+--keep-position} rev-range --revision "${scopeRevision:?}" --end-revision "${scopeEndRevision:?}" -2 format-patch RANGE "$@";;
+    repomove)
+	$EXEC git-branch-command "${branchCommandAdditionalArgs[@]}" --keep-position "${scopeCommand[@]}" ${scopeCommand:+--keep-position} rev-range --revision "${scopeRevision:?}" --end-revision "${scopeEndRevision:?}" reporangemove "$@";;
 
     @(st|files|submodules)?(mine|others|team))
 	$EXEC git-branch-command "${branchCommandAdditionalArgs[@]}" --keep-position "${scopeCommand[@]}" ${scopeCommand:+--keep-position} rev-range --revision "${scopeRevision:?}" --end-revision "${scopeEndRevision:?}" -2 "show$gitCommand" RANGE "$@";;

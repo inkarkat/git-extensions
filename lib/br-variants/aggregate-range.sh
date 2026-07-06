e8c8942cafb0890d6cbf265d916ce48a30f3848d
Ingo Karkat <swdev@ingo-karkat.de>
2026-07-06 11:09:49 +0200

    FIX: Move quotedArgs back to switch statement
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: git version 2.50.1, hub version 2.14.2, gh version 2.96.0 (2026-07-02), git-lfs/3.6.1 (GitHub; linux amd64; go 1.23.3)

--- lib/br-variants/aggregate-range.sh
+++ lib/br-variants/aggregate-range.sh
@@ -29,7 +29,6 @@ esac
 
 withAggregateFiles()
 {
-    local quotedArgs=; [ $# -eq 0 ] || printf -v quotedArgs ' %q' "$@"
     # FIXME: Extract FILE arguments and pass them to the source command.
     GIT_SELECTED_COMMAND_DEFAULT_FILES="GIT_REVRANGE_SEPARATE_ERRORS=t git-$scope files --no-header 2>/dev/null | sort --unique" \
 	$EXEC git-"$@"
@@ -73,6 +72,7 @@ set -- "${colorArg[@]}" "$@"
 
 case "$gitCommand" in
     ds)
+	quotedArgs=; [ $# -eq 0 ] || printf -v quotedArgs ' %q' "$@"
 	withAggregateFiles selected-command "$scope d${quotedArgs}";;
     dss)
 	withAggregateCommit --single-only dp "$@";;

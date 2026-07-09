1130e784133c8caa4eb7f1c438ae6a84a2cdd1bf
Ingo Karkat <swdev@ingo-karkat.de>
2026-07-09 10:32:58 +0200

    Refactoring: git-selected-command takes --command GIT-COMMAND or --exec GIT-COMMAND [...] ;
    This makes the argument passing a bit more robust (not that I suspect problems in the current implementation), and avoids some quoting in clients.
    Note: It still cannot adhere to the git-*-command interface of appending / --keep-position / -N / +N, because the file insertion always happens at the end and is controlled by commandOnSelected. And files / selected items will likely always be appended to the GIT-COMMAND, so it's also not an issue.
    
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: git version 2.50.1, hub version 2.14.2, gh version 2.96.0 (2026-07-02), git-lfs/3.6.1 (GitHub; linux amd64; go 1.23.3)

--- lib/br-variants/branch-range.sh
+++ lib/br-variants/branch-range.sh
@@ -193,14 +193,14 @@ activity?(except)by\
 	branchCommand --one-more-command 'greyonelineloghighlight lghighlight' --one-more-only-to-terminal -2 lghifiles RANGE "$@";;
     lghifiles)
 	GIT_SELECTED_COMMAND_DEFAULT_FILES="git-$scope files" \
-	    $EXEC git-selected-command "$scope lghipassedfiles" "$@";;
+	    $EXEC git-selected-command --exec "$scope" lghipassedfiles \; "$@";;
     lgfiles?(mine|others|team))
 	GIT_SELECTED_COMMAND_DEFAULT_FILES="git-$scope files${gitCommand#lgfiles}" \
-	    $EXEC git-selected-command "$scope lg${gitCommand#lgfiles}" "$@";;
+	    $EXEC git-selected-command --exec "$scope" "lg${gitCommand#lgfiles}" \; "$@";;
     lgfiles?(except)by)
 	quotedAuthorsAndRange="$(gitCommand=quoted othersCommand "$@")" || exit $?
 	GIT_SELECTED_COMMAND_DEFAULT_FILES="git-$scope files $quotedAuthorsAndRange" \
-	    $EXEC git-selected-command "onelinelog $quotedAuthorsAndRange --"
+	    $EXEC git-selected-command --command "onelinelog $quotedAuthorsAndRange --"
 	;;
     files@(l?(o)g|logv|lc|logfiles))
 	# Logs of files modified in the additions of the branch starting from before it.
@@ -296,11 +296,11 @@ revertcommit|\
 	othersCommand "$@";;
     changesetfileshere?(mine|others|team))
 	GIT_SELECTED_COMMAND_DEFAULT_FILES="git-$scope files${gitCommand#changesetfileshere}" \
-	    $EXEC git-selected-command "$scope ${gitCommand}passedfiles" "$@";;
+	    $EXEC git-selected-command --exec "$scope" "${gitCommand}passedfiles" \; "$@";;
     changesetfileshere?(except)by)
 	quotedAuthorsAndRange="$(gitCommand=quoted othersCommand "$@")" || exit $?
 	GIT_SELECTED_COMMAND_DEFAULT_FILES="git-$scope files $quotedAuthorsAndRange" \
-	    $EXEC git-selected-command "$scope changesetfilesherepassedfiles $quotedAuthorsAndRange --"
+	    $EXEC git-selected-command --command "$scope changesetfilesherepassedfiles $quotedAuthorsAndRange --"
 	;;
 
     emaillog)
